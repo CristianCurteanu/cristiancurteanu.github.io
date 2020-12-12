ce989e80db469d46e2e8c101b88fec40621f1a7d
CristianCurteanu <curteanucristian@yahoo.com>
2020-12-12 17:19:06 +0200

    [Update] Add changes to post pages

---
+++ _posts/2020-06-06-builder-pattern-js.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "How to implement Builder design pattern in JS"
+date:   2020-12-06 16:41:59 +0200
+description: "Various ways to implement the builder design pattern using JS structures"
+external_url: https://dev.to/cristicurteanu/how-to-implement-and-use-builder-pattern-in-js-1lda
+categories: ["Software engineering"]
+tags: ["js", "javascript", "design-patterns", "builder"]
+---
--- js/render_post.js
+++ js/render_post.js
@@ -1,12 +1,13 @@
 function renderPost(post) {
+  console.log('post.target', post.target)
   return `
     <div class="row shadow p-5">
-      <a href="${post.url}" class="text-dark">
+      <a href="${post.url}" target="${post.target}" class="text-dark">
         <h3>${post.title}</h3>
       </a>
       <p class="text-decoration-underline text-black-50">${post.date}</p>
       <p class="text-dark">${post.description}</p>
-      <a href="${post.url}">Read article</a>
+      <a href="${post.url}" target="${post.target}">Read article</a>
     </div>
   `
 }
